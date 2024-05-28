e42afd781fa23391f6a7ee5cfb9c889d15f8645a
devdops3 <devdops3@gmail.com>
2024-05-28 15:13:15 +0630

    upload new project

---
+++ BIS/App_Code/Service/EPaymentFactory.cs
@@ -0,0 +1,17 @@
+﻿public class EPaymentFactory
+{
+    
+    public IEPayment CreateEPayment(string ePaymentType)
+    {
+
+        switch (ePaymentType)
+        {
+            case BISConstants.MoMoneyPay:
+                return new MoMoney();
+            case BISConstants.APlus:
+                return new APlus();
+            default:
+                return null;
+        }
+    }
+}
